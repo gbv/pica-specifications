27d6b9fb9ec91a95e91df7b8666ee76486377bdb
Monty <Monty.Bitto@gbv.de>
2025-02-06 08:34:08 +0100

    Added some examples and a logo

---
+++ examples/example2.pp
@@ -0,0 +1,17 @@
+002@ $0Aau
+002C $aText$btxt
+002D $aohne Hilfsmittel zu benutzen$bn
+002E $aBand$bnc
+003@ $0879397667
+021A $aDeutsches Kirchenrecht
+028A $dAnita$adFriedrichs$BVerfasserIn$4aut
+028C $dThomas$aThöle$BHerausgeberIn$4edt
+041A $9104704861$8|g|Deutschland
+041A/01 $910428885X$8|s|Kirchenverfassung
+044K $9104704861$8|g|Deutschland
+044K $910428885X$8|s|Kirchenverfassung
+044K $zGeschichte
+044K/01 $9104704861$8|g|Deutschland
+044K/01 $910428885X$8|s|Kirchenverfassung
+044K/01 $zGeschichte 500-1500
+045Q/01 $9106422464$886.97 (Kirchenrecht)
