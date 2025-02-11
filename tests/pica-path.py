180f0511cf128d20d56985b2746e6c444c79d161
Monty <Monty.Bitto@gbv.de>
2025-02-11 11:46:27 +0100

    Added test in test suites

--- tests/pica-path.py
+++ tests/pica-path.py
@@ -1,11 +1,13 @@
 import csv
 import subprocess
 from pathlib import Path
+import json
 
 script_dir = Path(__file__).resolve().parent
 csv_file = script_dir / "pica-path.csv"
 records_dir = script_dir.parent / "examples"
-    
+results = []
+
 with open(csv_file, newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     next(reader) 
@@ -15,16 +17,13 @@ with open(csv_file, newline='', encoding='utf-8') as f:
         path, description, record, *expected = row
         expected_output = "\n".join(expected).strip()
             
-        print(description)
         record_path = records_dir / record            
         result = subprocess.run(["picadata", "-p", path, record_path], capture_output=True, text=True, check=True)
         actual_output = result.stdout.strip()
             
         if actual_output == expected_output:
-            print(f"OK {path}")
+            status = "passed"
         else:
-            print("---got:")
-            print(actual_output)
-            print("---expected:")
-            print(expected_output)
-        print()
+            status = "failed"
+        results.append({"name": path, "description": description, "status": status})
+print(json.dumps(results, indent=2))  
